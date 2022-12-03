a7b6d368a31ed56781bf1a407054e505e2995398
j-carson <44308120+j-carson@users.noreply.github.com>
2022-12-02 21:18:59 -0800

    day 3

---
+++ days/03/part2.py
@@ -0,0 +1,96 @@
+import sys
+from pathlib import Path
+
+import pytest
+from icecream import ic
+
+# --> Puzzle solution
+
+
+def parse(input_data):
+    data = input_data.splitlines()
+    while data:
+        yield data[:3]
+        data = data[3:]
+
+
+scores = dict(zip("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", range(1, 53)))
+
+
+def solve(input_data):
+    total = 0
+    for c1, c2, c3 in parse(input_data):
+        set1 = set(c1)
+        set2 = set(c2)
+        set3 = set(c3)
+        overlap = set1.intersection(set2).intersection(set3)
+        c = overlap.pop()
+        total += scores[c]
+
+    return total
+
+
+# --> Test driven development helpers
+
+# keep pytest ids smaller
+def idfn(maybe_string):
+    if isinstance(maybe_string, str):
+        # chop off long input strings in test name output
+        return maybe_string[:5].strip()
+    else:
+        return str(maybe_string)
+
+
+sample_data = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+
+def test_scores():
+    assert scores["p"] == 16
+    assert scores["L"] == 38
+    assert scores["P"] == 42
+    assert scores["v"] == 22
+
+
+# Test any examples given in the problem
+@pytest.mark.parametrize(
+    "sample_data,sample_solution",
+    [
+        (
+            """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+            70,
+        )
+    ],
+    ids=idfn,
+)
+def test_samples(sample_data, sample_solution) -> None:
+    assert solve(sample_data) == sample_solution
+
+
+# --> Setup and run
+
+if __name__ == "__main__":
+
+    #  Run the test examples with icecream debug-trace turned on
+    ic.enable()
+    ex = pytest.main([__file__, "--capture=tee-sys", "-v"])
+    if ex != pytest.ExitCode.OK and ex != pytest.ExitCode.NO_TESTS_COLLECTED:
+        print(f"tests FAILED ({ex})")
+        sys.exit(1)
+    else:
+        print("tests PASSED")
+
+    #  Actual input data generally has more iterations, turn off log
+    ic.disable()
+    my_input = Path("input.txt").read_text()
+    result = solve(my_input)
+    print(result)
